47650c15cb1c842eb6f5aa20e1e6aa3db2116193
VladimirBabin <vladimir.sol.rojo@gmail.com>
2023-11-13 16:12:48 +0200

    HTTP GET with Spring MVC List Operation

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -5,14 +5,23 @@ import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@Controller
+@RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1")
 public class BeerController {
     private final BeerService beerService;
+
+    @RequestMapping("/beers")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
         log.debug("Get Beer by Id - in controller");
